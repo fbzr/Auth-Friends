0aabf0e606118bb97d4d5b2c9cc849d0ed51d1a1
fbzr <fabricio.spb@gmail.com>
2020-03-24 10:07:16 -0700

    create login

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+// components
+import Login from './components/Login';
 
 function App() {
   return (
-    <div>
+    <Router>
       App
-    </div>
+      <Route exact path='/login'>
+        <Login />
+      </Route>
+    </Router>
   );
 }
 
